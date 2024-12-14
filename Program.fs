fcba0d5217557dda2ee7c11a745bcf0376c5a950
saraabbas123 <abbassara560@gmail.com>
2024-12-14 03:42:56 -0800

    Added new files to folder

---
+++ Program.fs
@@ -0,0 +1,8 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World!"
+    0 // return an integer exit code
