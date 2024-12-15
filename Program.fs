feb3e0cad2e579367ac5d484e8bab80740562f70
fatimaAlzahaa <fatooomalzahraa2023@gmail.com>
2024-12-15 20:45:59 +0200

    add borrow function

--- Program.fs
+++ Program.fs
@@ -6,3 +6,13 @@ open System
 let main argv =
     printfn "Hello World!"
     0 // return an integer exit code
+    // Function to borrow a book and automatically record the borrow date
+let borrowBook title =
+    match library.TryFind(title) with
+    | Some book when not book.IsBorrowed -> 
+        let updatedBook = { book with IsBorrowed = true; BorrowDate = Some DateTime.Now }
+        library <- library.Add(title, updatedBook)
+        saveLibraryToFile ()
+        "Book borrowed successfully"
+    | Some book -> "This book is already borrowed"
+    | None -> "Book not found"
